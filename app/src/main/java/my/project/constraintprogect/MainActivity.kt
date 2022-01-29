9407711bbe3eed026774cf3ef5d185c328333c6f
Urmaturi <urmasuper93@gmail.com>
2022-01-29 22:48:52 +0600

    secong commit

--- app/src/main/java/my/project/constraintprogect/MainActivity.kt
+++ app/src/main/java/my/project/constraintprogect/MainActivity.kt
@@ -2,10 +2,50 @@ package my.project.constraintprogect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import my.project.constraintprogect.databinding.MainActivityBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var bindings:   ActivityMainBinding
+    private lateinit var bindings: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        //setContentView(R.layout.main_activity)
+        bindings = DataBindingUtil.setContentView(this,R.layout.main_activity)
+       /*findViewById<Button>(R.id.button_done).setOnClickListener {
+            addNickname(it)
+
+        }*/
+        bindings.buttonDone.setOnClickListener{addNickname(it)}
     }
+
+    private fun addNickname(view: View)
+    {
+        //val editText = findViewById<EditText>(R.id.name_adit)
+        //val nicknameTextView = findViewById<TextView>(R.id.textViewNickName)
+        // nicknameTextView.text = editText.text
+       //  editText.visibility = View.GONE
+        //nicknameTextView.visibility = View.VISIBLE
+        bindings.apply {
+        bindings.textViewNickName.text = bindings.nameAdit.text
+            invalidateAll()
+        bindings.nameAdit.visibility = View.GONE
+        bindings.textViewNickName.visibility = View.VISIBLE
+
+        }
+
+
+
+    }
+
+
+
+
 }
